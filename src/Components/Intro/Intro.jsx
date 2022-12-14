b8743789408b40d98146a480324d75467ecd5277
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-12-14 11:59:27 -0300

    add icons

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Intro.css'
 
+import { AiOutlineHtml5 } from 'react-icons/ai'
+import { DiCss3 } from 'react-icons/di'
+import { IoLogoJavascript } from 'react-icons/io'
+import { FaReact } from 'react-icons/fa'
+
 const Intro = () => {
   return (
     <div className='intro'>
@@ -9,13 +14,21 @@ const Intro = () => {
           <span>Oi, eu sou</span>
           <span>Márcio Reis</span>
           <span>Desenvolvedor Front-end</span>
-          <span>Tenho paixão por tecnologia, e ela me faz ter vontade de trabalhar e estudar cada vez mais para solucionar os mais variados tipos de problemas na área de desenvolvimento.
-          Seguir este caminho tem sido desafiador e me faz querer sempre o melhor para meus clientes. Dar uma solução inovadora para seus problemas e suas necessidades é o que me motiva, além de aprender, claro!
-          Estou sempre estudando e aprendendo, e no momento estudo e trabalho com: HTML, CSS, JavaScript, React e Angular.</span>
+          <p>Tenho paixão por tecnologia, e ela me faz ter vontade de trabalhar e estudar cada vez mais para solucionar os mais variados tipos de problemas na área de desenvolvimento.</p>
+          <p>Seguir este caminho tem sido desafiador e me faz querer sempre o melhor para meus clientes. Dar uma solução inovadora para seus problemas e suas necessidades é o que me motiva, além de aprender, claro!</p>
+          <p>Estou sempre estudando e aprendendo, e no momento estudo e trabalho com: HTML, CSS, JavaScript e React.</p>
+          <i className='intro-icons'>
+            <AiOutlineHtml5 />
+            <DiCss3 />
+            <IoLogoJavascript />
+            <FaReact />
+          </i>
         </div>
         <button className="button intro-button">Fale comigo</button>
       </div>
-      <div className="intro-right"></div>
+      <div className="intro-right">
+        marcio
+      </div>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,10 +10,9 @@ const Navbar = () => {
       <div className="nav-right">
         <div className="nav-list">
           <ul>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
+            <li><i class="uil uil-home"></i> Home</li>
+            <li><i class="uil uil-user"></i> Sobre</li>
+            <li><i class="uil uil-bag"></i> Portfólio</li>
           </ul>
         </div>
         <button className="button nav-button">Currículo</button>
