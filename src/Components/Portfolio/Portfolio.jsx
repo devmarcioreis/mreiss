548a08f88d0f11a33d52eee144065dbbc2c08113
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-12-16 11:47:57 -0300

    new img

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import './Portfolio.css'
+
+import Music from '../../img/music.jpg'
+import Restautant from '../../img/restaurant.jpg'
+import Space from '../../img/space.png'
+import Sports from '../../img/sports.jpg'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className="portfolio">
+        <h1>Portfólio</h1>
+        <span>últimos Projetos</span>
+        <Swiper>
+            <SwiperSlide>
+                <img src={Music} alt="Music" />
+            </SwiperSlide>
+        </Swiper>
+    </div>
   )
 }
 
