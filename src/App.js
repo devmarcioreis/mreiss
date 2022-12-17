b5886a58106a86227f3f0532fade8f8f207ddeee
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-12-17 10:24:43 -0300

    add in App js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css'
 import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
 import Intro from './Components/Intro/Intro';
 import Navbar from './Components/Navbar/Navbar'
 import Portfolio from './Components/Portfolio/Portfolio';
@@ -12,6 +14,8 @@ const App = () => {
         <Intro />
         <About />
         <Portfolio />
+        <Contact />
+        <Footer />
       </div>
     </>
   )
