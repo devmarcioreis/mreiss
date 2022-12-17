389e11ac12c309a76b175acc542bed3bfae7fce4
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-12-17 10:33:20 -0300

    remove section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css'
 import About from './Components/About/About';
-import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
 import Intro from './Components/Intro/Intro';
 import Navbar from './Components/Navbar/Navbar'
@@ -14,7 +13,6 @@ const App = () => {
         <Intro />
         <About />
         <Portfolio />
-        <Contact />
         <Footer />
       </div>
     </>
